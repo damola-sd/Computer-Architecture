a8fd370c23d9ec4e0338925fc5d4530f7f18cdd1
Damola Adewunmi <damolasd@gmail.com>
2019-11-12 17:15:12 +0100

    Loaded file

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """Main."""
 
 import sys
@@ -7,5 +6,12 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+
+if len(sys.argv) != 2:
+    print("usage: ls8.py <filename>", file=sys.stderr)
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+cpu.load(filename)
 cpu.run()
